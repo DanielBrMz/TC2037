b64c8ab269bcfbd9ba78f2755a756215ea5c9f18
Daniel Barreras <112592126+DanielBrMz@users.noreply.github.com>
2024-03-27 23:00:36 -0700

    Update main.rs

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -191,10 +191,5 @@ fn token_type(token: &Token) -> &str {
 
 fn main() {
     let filepath = "expressions.txt";
-    lexer(filepath);
-    println!("Hello, World!");
-    println!("Hello, World!");
-    println!("Hello, World!");
-    println!("Hello, World!");
-   
+    lexer(filepath);   
 }
